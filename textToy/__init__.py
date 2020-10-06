51f8e3b6f1f6a0993bf1414703e99c66c42463bf
huanghuidmml <m13021933043@163.com>
2020-10-06 13:26:54 +0800

    修复trainer

--- textToy/__init__.py
+++ textToy/__init__.py
@@ -5,7 +5,6 @@
 @date: 2020/09/08
 """
 
-from .trainer import Trainer
 from .config import (
     BertConfig, ALBertConfig, ElectraConfig, NeZhaConfig, CONFIGS)
 from .tokenizer import BertTokenizer, ALBertTokenizer, ElectraTokenizer, NeZhaTokenizer, WoBertTokenizer, TOKENIZERS
@@ -14,3 +13,4 @@ from .ptm import (
     SequenceClassification, TokenClassification, MODELS,
     PTMExtractFeature, MultiLabelClassification, PretrainingModel)
 from .utils import ProgressBar, set_seed
+from .trainer import Trainer
