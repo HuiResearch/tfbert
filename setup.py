6a505516830c85d580ddb03be626526f02b7b1e0
huanghuidmml <45792773+huanghuidmml@users.noreply.github.com>
2020-12-18 12:34:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="textToy",
     version="0.0.2",
     author="huanghui",
-    author_email="m130219330432163.com",
+    author_email="m13021933043@163.com",
     description="A simple tool for pretrain model",
     license="Apache",
     url="https://github.com/huanghuidmml/textToy",
